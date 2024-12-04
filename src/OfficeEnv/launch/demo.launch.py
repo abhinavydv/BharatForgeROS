400ca92d8bb32c599a399996da9fd532fb56651f
AbhinavYdv <abhinavyadavdev@gmail.com>
2024-12-04 16:45:36 +0000

    Fixed: loop in tf

--- src/OfficeEnv/launch/demo.launch.py
+++ src/OfficeEnv/launch/demo.launch.py
@@ -87,10 +87,6 @@ def generate_launch_description():
             package='tf2_ros', executable='static_transform_publisher', output='screen', 
             arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', f'{robot}/kinect', f'{robot}/base_link']
         ),)
-        nodes.append(Node(
-             package='tf2_ros', executable='static_transform_publisher', output='screen', 
-             arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', f'{robot}/camera_link_optical', f'{robot}/base_link']
-        ))
 
 
     return LaunchDescription([
