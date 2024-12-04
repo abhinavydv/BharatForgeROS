3f9b61955ad18eac39b9c13bb1974c2a70b2571d
AbhinavYdv <abhinavyadavdev@gmail.com>
2024-12-04 16:29:57 +0000

    Multiple bots depth launch working

--- src/OfficeEnv/launch/depth.launch.py
+++ src/OfficeEnv/launch/depth.launch.py
@@ -8,6 +8,8 @@ from launch_ros.substitutions import FindPackageShare
 
 pkg_name = "OfficeEnv"
 
+NUM_ROBOTS = 2
+
 def generate_launch_description():
     urdf_path = PathJoinSubstitution(
         [FindPackageShare(pkg_name), "urdf", "my_robot.urdf.xacro"]
@@ -15,6 +17,38 @@ def generate_launch_description():
     rviz_config_path = PathJoinSubstitution(
         [FindPackageShare(pkg_name), "rviz", "robot_rviz_config.rviz"]
     )
+
+    robots = []
+    spawns = []
+    tf_s = []
+
+    for i in range(NUM_ROBOTS):
+        robots.append(
+            Node(
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                parameters=[{
+                    'robot_description': Command(['xacro ', LaunchConfiguration('urdf_path'), f" robot_name:=robot_{i}"])
+                }],
+                remappings=[('/tf', f'/robot_{i}/tf'), ("/robot_description", f"/robot_{i}/robot_description")],
+            ),
+        )
+
+        spawns.append(
+            Node(
+                package='gazebo_ros',
+                executable='spawn_entity.py',
+                arguments=['-topic', f'robot_{i}/robot_description', '-robot_namespace', f'/robot_{i}', '-entity', f'robot_{i}', '-x', str(-i), '-y', '1.0', '-z', '0.0']
+            )
+        )
+
+        tf_s.append(
+            Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                arguments=[str(-i), '0.0', '0.0', '0.0', '0.0', '0.0', '/world', f'/robot_{i}/odom']
+            )
+        )
 
     return LaunchDescription([
 
@@ -36,30 +70,13 @@ def generate_launch_description():
             output='screen',
             arguments=['-d', LaunchConfiguration('rviz_config_path')]
         ),
-        # Node(
-        #     package=pkg_name,
-        #     executable='image_saver'
-        # ),
-        Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui'
-        ),
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            parameters=[{
-                'robot_description': Command(['xacro ', LaunchConfiguration('urdf_path')])
-            }]
-        ),
+        *tf_s,
+        *robots,
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([
                 PathJoinSubstitution([FindPackageShare('gazebo_ros'), 'launch', 'gazebo.launch.py'])
             ]),
             launch_arguments={'world': PathJoinSubstitution([FindPackageShare(pkg_name), 'worlds', 'updated_office.world'])}.items()
         ),
-        Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            arguments=['-topic', 'robot_description', '-entity', 'my_robot', '-x', '1.0', '-y', '1.0', '-z', '0.0']
-        )
+        *spawns,
     ])
